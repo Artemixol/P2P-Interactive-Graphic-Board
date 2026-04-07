65a73743a657122b4edb8fea96b5efc34bb897cb
Artemixol <artemixol@xmail.ru>
2026-04-07 19:10:17 +0300

    Добавлена проверка разрешений и работающий поиск узлов сети

--- app/src/main/java/com/example/interactive_graphic_board/FindSessionActivity.java
+++ app/src/main/java/com/example/interactive_graphic_board/FindSessionActivity.java
@@ -1,34 +1,42 @@
 package com.example.interactive_graphic_board;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.wifi.WpsInfo;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.RequiresPermission;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class FindSessionActivity extends AppCompatActivity {
 
-    private WifiP2pManager manager;
-    private WifiP2pManager.Channel channel;
-    private WiFiDirectBroadcastReceiver receiver;
-
-    private final IntentFilter intentFilter = new IntentFilter();
-
-    private Boolean isWifiP2pEnabled;
+    /*
+        Жизненный цикл активити
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +49,10 @@ public class FindSessionActivity extends AppCompatActivity {
             return insets;
         });
 
-        addActions(); // добавление отслеживаемых событий
-        createWifiManager(); // создание Wifi менеджера и канала
+        setAdapterInListView();
+        checkAndRequestPermissions();
+        addActions();
+        createWifiManager();
     }
 
     @RequiresPermission(allOf = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.NEARBY_WIFI_DEVICES}) // проверка разрешений
@@ -51,7 +61,7 @@ public class FindSessionActivity extends AppCompatActivity {
         super.onResume();
 
         // Создание приёмника и его регистрация в активити
-        receiver = new WiFiDirectBroadcastReceiver(manager, channel, this);
+        receiver = new WiFiDirectBroadcastReceiver(manager, channel, this, peerListListener);
         registerReceiver(receiver, intentFilter);
 
         startSearchPeers(); // поиск узлов в одноранговой сети
@@ -64,18 +74,65 @@ public class FindSessionActivity extends AppCompatActivity {
         unregisterReceiver(receiver); // конец вещания
     }
 
-    // Метод, добавляющий элементы ArrayList в ListView
-    public void setDataInListView(ArrayList<String> sessionsList) {
+    /*
+        Отображение интерфейса
+     */
+
+    // Поле со списком устройств, доступных для подключения
+    // Устанавливается один раз в setAdapterInListView()
+    // Обновляется методом onPeersAvailable() объекта PeerListListener
+    ArrayAdapter<WifiP2pDevice> arrayAdapterDevices;
+
+    // Метод для установки ArrayAdapter в ListView
+    public void setAdapterInListView() {
         ListView listView = (ListView) findViewById(R.id.scrollRoomsListView);
 
-        // Объект-адаптер с указанием контекста, интерфейса для отображения элементов
-        // и массива самих элементов
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, sessionsList);
+        // Изначально пустой список устройств
+        ArrayList<WifiP2pDevice> devices = new ArrayList<WifiP2pDevice>();
 
-        listView.setAdapter(adapter);
+        // Инициализация ArrayAdapter с элементами WifiP2pDevice
+        arrayAdapterDevices = new ArrayAdapter<WifiP2pDevice>(this,
+                android.R.layout.simple_list_item_1, devices);
+
+        // Установка адаптера в ListView
+        listView.setAdapter(arrayAdapterDevices);
     }
 
+    // Метод для обновления данных в адаптере
+    public void updateAdapter(Collection<WifiP2pDevice> devices) {
+        arrayAdapterDevices.clear();
+        arrayAdapterDevices.addAll(devices);
+        arrayAdapterDevices.notifyDataSetChanged();
+    }
+
+    public void GoBack(View v){
+        /*Возвращение на MainActivity*/
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    // Переход на графическую доску
+    @RequiresPermission(allOf = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.NEARBY_WIFI_DEVICES})
+    public void GoToBoard(View v){
+
+        // TODO Реализовать подключение к выбранному пользователем устройству и при успешном: отправление пароля и переход на доску
+        // connectToDevice();
+
+//        Intent intent = new Intent(this, BoardActivity.class);
+//        startActivity(intent);
+    }
+
+    /*
+        Поиск устройств
+     */
+
+    private WifiP2pManager manager;
+    private WifiP2pManager.Channel channel;
+    private WiFiDirectBroadcastReceiver receiver;
+
+    private final IntentFilter intentFilter = new IntentFilter();
+
+    // Метод для добавления отслеживаемых событий
     public void addActions() {
         Log.d(this.getClass().getSimpleName(), "addActions");
 
@@ -85,6 +142,7 @@ public class FindSessionActivity extends AppCompatActivity {
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
     }
 
+    // Создание Wifi Direct менеджера и канала
     public void createWifiManager() {
         Log.d(this.getClass().getSimpleName(), "createWifiManager");
 
@@ -92,6 +150,7 @@ public class FindSessionActivity extends AppCompatActivity {
         channel = manager.initialize(this, getMainLooper(), null);
     }
 
+    // Метод для поиска узлов (пиров) P2P сети
     @RequiresPermission(allOf = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.NEARBY_WIFI_DEVICES}) // проверка разрешений
     public void startSearchPeers() {
         Log.d(this.getClass().getSimpleName(), "startSearchPeers");
@@ -101,18 +160,124 @@ public class FindSessionActivity extends AppCompatActivity {
 
             @Override
             public void onSuccess() { // успех
-                Log.d(this.getClass().getSimpleName(), "onSuccess");
+                Log.d("FindSessionActivity", "onSuccess");
             }
 
             @Override
             public void onFailure(int reasonCode) { // неудача
-                Log.d(this.getClass().getSimpleName(), "onFailure");
+                Log.d("FindSessionActivity", "onFailure:" + reasonCode);
             }
         });
     }
-    public void GoBack(View v){
-        /*Возвращение на MainActivity*/
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+
+    // Поле с реализацией интерфейса PeerListListener
+    private WifiP2pManager.PeerListListener peerListListener = new WifiP2pManager.PeerListListener() {
+        @Override
+        public void onPeersAvailable(WifiP2pDeviceList peerList) { // вызов, когда список устройств готов
+            Log.d(this.getClass().getSimpleName(), "peerListListener.onPeersAvailable");
+
+            Collection<WifiP2pDevice> devices = peerList.getDeviceList();
+
+            updateAdapter(devices);
+        }
+    };
+
+    // Метод для подключения к устройству (узлу)
+    @RequiresPermission(allOf = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.NEARBY_WIFI_DEVICES})
+    private void connectToDevice(WifiP2pDevice device) {
+        Log.d(this.getClass().getSimpleName(), "connectToDevice");
+
+        // Создание объекта конфигурации
+        WifiP2pConfig config = new WifiP2pConfig();
+        config.deviceAddress = device.deviceAddress; // MAC-адрес целевого устройства
+
+        // Запрос системе на соединение
+        manager.connect(channel, config, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                Log.d("WiFiDirect", "Запрос на подключение успешно отправлен");
+            }
+
+            @Override
+            public void onFailure(int reason) {
+                Log.e("WiFiDirect", "Не удалось отправить запрос на подключение. Код ошибки: " + reason);
+            }
+        });
+    }
+
+    /*
+        Проверка разрешений
+     */
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+
+    // Проверка разрешений для приложения
+    private void checkAndRequestPermissions() {
+        String[] permissions;
+
+        // Для Android 13 (API 33) и выше
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+            permissions = new String[] {
+                    Manifest.permission.NEARBY_WIFI_DEVICES
+            };
+        }
+        // Для более старых версий
+        else {
+            permissions = new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            };
+        }
+
+        // Проверка, все ли разрешения уже предоставлены
+        boolean allGranted = true;
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                allGranted = false;
+                break;
+            }
+        }
+
+        // Если какое-то разрешение не предоставлено
+        if (!allGranted) {
+            if (shouldShowRequestPermissionRationale(permissions[0])) {
+                new AlertDialog.Builder(this)
+                        .setTitle("Необходимо разрешение")
+                        .setMessage("Для поиска устройств по Wi-Fi Direct требуется разрешение.")
+                        .setPositiveButton("OK", (dialog, which) -> {
+                            ActivityCompat.requestPermissions(this, permissions, PERMISSIONS_REQUEST_CODE);
+                            // после нажатия на кнопку - запуск onRequestPermissionsResult
+                        })
+                        .setNegativeButton("Отмена", null)
+                        .show();
+            } else {
+                // Запрашиваем разрешения напрямую
+                ActivityCompat.requestPermissions(this, permissions, PERMISSIONS_REQUEST_CODE);
+            }
+        } else {
+            // Все разрешения уже есть
+        }
+    }
+
+    // Обработка результата запроса разрешений
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSIONS_REQUEST_CODE) {
+            // Проверка, все ли запрошенные разрешения были предоставлены
+            boolean allGranted = true;
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    allGranted = false;
+                    break;
+                }
+            }
+
+            if (allGranted) {
+                // Разрешения получены
+            } else {
+                // Разрешения не получены
+                Toast.makeText(this, "Для работы приложения необходимы разрешения", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/interactive_graphic_board/WiFiDirectBroadcastReceiver.java
+++ app/src/main/java/com/example/interactive_graphic_board/WiFiDirectBroadcastReceiver.java
@@ -1,11 +1,16 @@
 package com.example.interactive_graphic_board;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.NetworkInfo;
 import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.util.Log;
+
+import androidx.annotation.RequiresPermission;
 
 public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
 
@@ -14,42 +19,52 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
 
     Context activity;
 
-    public WiFiDirectBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel, Context activity) {
+    private final WifiP2pManager.PeerListListener peerListListener;
+
+    public WiFiDirectBroadcastReceiver(
+            WifiP2pManager manager,
+            WifiP2pManager.Channel channel,
+            Context activity,
+            WifiP2pManager.PeerListListener peerListListener
+    ) {
         this.manager = manager;
         this.channel = channel;
         this.activity = activity;
+        this.peerListListener = peerListListener;
     }
 
+    @RequiresPermission(allOf = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.NEARBY_WIFI_DEVICES})
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
-        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) { // изменение состояния Wi-Fi P2P: включён или отключён
             Log.d(this.getClass().getSimpleName(), "onReceive:WIFI_P2P_STATE_CHANGED_ACTION");
-            // Determine if Wi-Fi Direct mode is enabled or not, alert
-            // the Activity.
-//            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
-//            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
-//                activity.setIsWifiP2pEnabled(true);
-//            } else {
-//                activity.setIsWifiP2pEnabled(false);
-//            }
-        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) { // список пиров изменился
             Log.d(this.getClass().getSimpleName(), "onReceive:WIFI_P2P_PEERS_CHANGED_ACTION");
-            // The peer list has changed! We should probably do something about
-            // that.
 
-        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            // Запрос списка у менеджера
+            if (manager != null) {
+                manager.requestPeers(channel, peerListListener);
+            }
+
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) { // изменилось состояние подключения к узлу
             Log.d(this.getClass().getSimpleName(), "onReceive:WIFI_P2P_CONNECTION_CHANGED_ACTION");
-            // Connection state changed! We should probably do something about
-            // that.
 
-        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            // Получение информации о текущем соединении
+            WifiP2pInfo connectionInfo = intent.getParcelableExtra(WifiP2pManager.EXTRA_WIFI_P2P_INFO);
+            // Получение информации о сетевом интерфейсе
+            NetworkInfo networkInfo = intent.getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
+
+            if (connectionInfo != null && networkInfo != null && networkInfo.isConnected()) {
+                // TODO Реализовать передачу данных через сокеты (для начала - пароля)
+            } else {
+                // Соединение разорвано или не было установлено
+            }
+
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) { // изменение сведений об устройстве
             Log.d(this.getClass().getSimpleName(), "onReceive:WIFI_P2P_THIS_DEVICE_CHANGED_ACTION");
-//            DeviceListFragment fragment = (DeviceListFragment) activity.getFragmentManager()
-//                    .findFragmentById(R.id.frag_list);
-//            fragment.updateThisDevice((WifiP2pDevice) intent.getParcelableExtra(
-//                    WifiP2pManager.EXTRA_WIFI_P2P_DEVICE));
-
         }
     }
 }
