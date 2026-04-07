ebf12b35b07209920bfb76a23450895005d1a157
Vitaly Popov <popovvit06@yandex.ru>
2026-04-07 18:57:40 +0300

    Реализация функциоанала создания комнаты (CreateSessionActivity, RoomManager, создание CanvasActivity)

--- app/src/main/java/com/example/interactive_graphic_board/CreateSessionActivity.java
+++ app/src/main/java/com/example/interactive_graphic_board/CreateSessionActivity.java
@@ -3,6 +3,8 @@ package com.example.interactive_graphic_board;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,7 +24,35 @@ public class CreateSessionActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+       EditText room_name_input_field = findViewById(R.id.room_name_input_field);
+       EditText password_field = findViewById(R.id.password_field);
+       Button create_canvas = findViewById(R.id.create_canvas);
+
+       create_canvas.setOnClickListener(v -> {
+           String roomName = room_name_input_field.getText().toString().trim();
+           String roomPassword = password_field.getText().toString().trim();
+           if (roomName.isEmpty()) {
+               room_name_input_field.setError("Введите название комнаты");
+               return;
+           }
+           if (roomPassword.isEmpty()) {
+               password_field.setError("Введите пароль");
+               return;
+           }
+
+
+           RoomManager.getInstance().createRoom(roomName, roomPassword);
+
+           Intent intent = new Intent(this, CanvasActivity.class);
+           startActivity(intent);
+           finish();
+       });
+
     }
+
+
+
     public void GoBack(View v){
         /*Возвращение на MainActivity*/
         Intent intent = new Intent(this, MainActivity.class);
