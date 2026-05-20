2eae8ed90ef311726ebc64aa42bf8f73beee2bfd
Vitaly Popov <popovvit06@yandex.ru>
2026-05-20 16:35:07 +0300

    Добавил реализацию холста, рисование кривых, полное очищение экрана

--- app/src/main/java/com/example/interactive_graphic_board/CanvasActivity.java
+++ app/src/main/java/com/example/interactive_graphic_board/CanvasActivity.java
@@ -1,6 +1,8 @@
 package com.example.interactive_graphic_board;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class CanvasActivity extends AppCompatActivity {
+
+    private DrawingView drawingView;
+    private TextView tvRoomInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,15 @@ public class CanvasActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        drawingView = findViewById(R.id.drawingView);
+        tvRoomInfo = findViewById(R.id.tvRoomInfo);
+        Button btnClear = findViewById(R.id.btnClear);
+
+        String roomName = RoomManager.getInstance().getRoomName();
+        tvRoomInfo.setText("Комната: " + roomName + " (хост)");
+
+        btnClear.setOnClickListener(v -> drawingView.clearCanvas());
+
     }
 }
--- app/src/main/java/com/example/interactive_graphic_board/DrawingView.java
+++ app/src/main/java/com/example/interactive_graphic_board/DrawingView.java
@@ -1,4 +1,75 @@
 package com.example.interactive_graphic_board;
 
-public class DrawingView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class DrawingView extends View {
+    private Paint paint;
+    private Path path;
+    private float lastX, lastY;
+
+
+    public DrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+
+    private void init() {
+        paint = new Paint();
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(10f);
+        paint.setAntiAlias(true);
+        path = new Path();
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(x, y);
+                lastX = x;
+                lastY = y;
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                float dx = Math.abs(x - lastX);
+                float dy = Math.abs(y - lastY);
+                if (dx > 4 || dy > 4) {
+                    path.quadTo(lastX, lastY, (x + lastX)/2, (y + lastY)/2);
+                    lastX = x;
+                    lastY = y;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                path.lineTo(x, y);
+                break;
+        }
+
+        invalidate();
+        return true;
+    }
+
+
+    @Override
+    protected void onDraw(Canvas cnvs) {
+        super.onDraw(cnvs);
+        cnvs.drawPath(path, paint);
+    }
+
+
+    public void clearCanvas() {
+        path.reset();
+        invalidate();
+    }
 }
